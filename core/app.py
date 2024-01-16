c1d5272844cc5be87d1128ddd9b4040d89320171
dolphin4438 <www.trunghieu314@gmail.com>
2024-01-17 05:48:22 +0700

    added: GET to POST request in Flask appp, close #2

--- core/app.py
+++ core/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from markupsafe import escape
 from src.rag.hybrid_gemini import HybridGeminiRag
 from src.config import Configuration
+import logging
+
+# When call API, please check the "status" field first
+# 200 is success, else is error
 
 app = Flask(__name__)
 
@@ -19,10 +23,26 @@ def hello_world():
     data = {"message": WELCOME, "status": 200}
     return data
 
-@app.route("/qa/<question>")
-def answer_major(question):
-    answer = RAG.rag.invoke(escape(question))
-    data = {"question": question, "answer": answer, "status": 200}
+@app.route("/test_qa/")
+def test_answer_major():
+    #question = "This is user input question"
+    answer = "This is a sample json result"
+    data = {"question": "What is life", "answer": answer, "status": 200}
+    return data
+
+@app.route("/qa", methods=["POST"])
+def answer_major():
+    if request.method != "POST":
+        return {"status": 404}
+    try:
+        question = request.form.get("question")
+        answer = RAG.rag.invoke(escape(question))
+        #answer = "tôi không biết thông tin chi tiết"
+        data = {"question": question, "answer": answer, "status": 200}
+    except Exception as Argument:
+        with open("rag.log", "a") as f:
+            f.write(f"[ERROR] {str(Argument)}")
+        return {"status": 404}
     return data
 
 if __name__ == '__main__':
---
+++ test_POST.sh
@@ -0,0 +1,1 @@
+echo -e `curl -d "question=Ngành ngôn ngữ Anh ra trường làm gì?" http://127.0.0.1:5000/qa`
