e8d720cf7a1276ebcf158a566affb8ed21c55938
dolphin4438 <www.trunghieu314@gmail.com>
2024-01-28 23:18:44 +0700

    updated: test POST bash

--- test_POST.sh
+++ test_POST.sh
@@ -3,5 +3,5 @@ QUES=""
 read QUES
 if [[ -z "$QUES" ]]; then
 	QUES="tư vấn ngành CNTT"
-echo -e `curl -d "question=$QUES" http://127.0.0.1:5000/qa`
 fi
+echo -e `curl -d "question=$QUES" http://127.0.0.1:5000/qa`
