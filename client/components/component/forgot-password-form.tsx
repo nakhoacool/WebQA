f2a7554d0aac565d7ade8c7904f390a55a07ec9c
Khoa Nguyen <77623180+nakhoacool@users.noreply.github.com>
2024-02-13 09:58:07 +0700

    Add react-toastify for displaying toast messages

--- client/components/component/forgot-password-form.tsx
+++ client/components/component/forgot-password-form.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { auth } from '@/lib/firebase/config'
 import { useRouter } from 'next/navigation'
 import { sendPasswordResetEmail } from 'firebase/auth'
+import { Bounce, ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function ForgotPasswordForm() {
   const [email, setEmail] = useState('')
@@ -10,64 +12,86 @@ export default function ForgotPasswordForm() {
   const resetEmail = () => {
     sendPasswordResetEmail(auth, email)
       .then(() => {
-        router.replace('/auth/signin')
+        toast.success('Password reset email sent', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: false,
+          progress: undefined,
+          theme: 'light',
+          transition: Bounce,
+          onClose: () => router.replace('/auth/signin'),
+        })
       })
       .catch((error) => {
-        const errorCode = error.code
-        const errorMessage = error.message
-        alert(errorMessage)
+        toast.error(error.message, {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+          transition: Bounce,
+        })
       })
   }
 
   return (
-    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
-      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-        <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white'>
-          Forgot Password
-        </h2>
-      </div>
+    <div>
+      <ToastContainer />
+      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+          <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white'>
+            Forgot Password
+          </h2>
+        </div>
 
-      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-        <div className='space-y-6'>
-          <div>
-            <label
-              htmlFor='email'
-              className='block text-sm font-medium leading-6 text-white'
-            >
-              Email address
-            </label>
-            <div className='mt-2'>
-              <input
-                id='email'
-                name='email'
-                type='email'
-                autoComplete='email'
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
-              />
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+          <div className='space-y-6'>
+            <div>
+              <label
+                htmlFor='email'
+                className='block text-sm font-medium leading-6 text-white'
+              >
+                Email address
+              </label>
+              <div className='mt-2'>
+                <input
+                  id='email'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                onClick={() => resetEmail()}
+                disabled={!email}
+                className='disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+              >
+                Send Forgot Password Email
+              </button>
             </div>
           </div>
-
-          <div>
+          <p className='mt-10 text-center text-sm text-gray-400'>
+            Remember ?{' '}
             <button
-              onClick={() => resetEmail()}
-              disabled={!email}
-              className='disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+              onClick={() => router.replace('/auth/signin')}
+              className='font-semibold leading-6 text-indigo-400 hover:text-indigo-300'
             >
-              Send Forgot Password Email
+              Sign In
             </button>
-          </div>
+          </p>
         </div>
-        <p className='mt-10 text-center text-sm text-gray-400'>
-          Remember ?{' '}
-          <button
-            onClick={() => router.replace('/auth/signin')}
-            className='font-semibold leading-6 text-indigo-400 hover:text-indigo-300'
-          >
-            Sign In
-          </button>
-        </p>
       </div>
     </div>
   )
--- client/components/component/signin-form.tsx
+++ client/components/component/signin-form.tsx
@@ -1,101 +1,122 @@
 'use client'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import { Bounce, ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 export default function SignInForm() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const router = useRouter()
 
+  useEffect(() => {
+    const toastSuccess = localStorage.getItem('signupSuccess')
+    if (toastSuccess) {
+      toast.success(toastSuccess, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+        transition: Bounce,
+        onClose: () => localStorage.removeItem('signupSuccess'),
+      })
+    }
+  }, [])
+
   return (
-    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
-      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-        <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white'>
-          Sign in to your account
-        </h2>
-      </div>
-
-      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-        <div className='space-y-6'>
-          <div>
-            <label
-              htmlFor='email'
-              className='block text-sm font-medium leading-6 text-white'
-            >
-              Email address
-            </label>
-            <div className='mt-2'>
-              <input
-                id='email'
-                name='email'
-                type='email'
-                autoComplete='email'
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className='flex items-center justify-between'>
+    <div>
+      <ToastContainer />
+      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+          <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white'>
+            Sign in to your account
+          </h2>
+        </div>
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+          <div className='space-y-6'>
+            <div>
               <label
-                htmlFor='password'
+                htmlFor='email'
                 className='block text-sm font-medium leading-6 text-white'
               >
-                Password
+                Email address
               </label>
-              <div className='text-sm'>
-                <div
-                  onClick={() => router.push('/auth/forgot-password')}
-                  className='cursor-pointer font-semibold text-indigo-400 hover:text-indigo-300'
-                >
-                  Forgot password?
-                </div>
+              <div className='mt-2'>
+                <input
+                  id='email'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
+                />
               </div>
             </div>
-            <div className='mt-2'>
-              <input
-                id='password'
-                name='password'
-                type='password'
-                autoComplete='current-password'
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
-              />
+
+            <div>
+              <div className='flex items-center justify-between'>
+                <label
+                  htmlFor='password'
+                  className='block text-sm font-medium leading-6 text-white'
+                >
+                  Password
+                </label>
+                <div className='text-sm'>
+                  <div
+                    onClick={() => router.push('/auth/forgot-password')}
+                    className='cursor-pointer font-semibold text-indigo-400 hover:text-indigo-300'
+                  >
+                    Forgot password?
+                  </div>
+                </div>
+              </div>
+              <div className='mt-2'>
+                <input
+                  id='password'
+                  name='password'
+                  type='password'
+                  autoComplete='current-password'
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                onClick={() =>
+                  signIn('credentials', {
+                    email,
+                    password,
+                    redirect: true,
+                    callbackUrl: '/',
+                  })
+                }
+                disabled={!email || !password}
+                className='disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+              >
+                Sign in
+              </button>
             </div>
           </div>
 
-          <div>
+          <p className='mt-10 text-center text-sm text-gray-400'>
+            Not a member?{' '}
             <button
-              onClick={() =>
-                signIn('credentials', {
-                  email,
-                  password,
-                  redirect: true,
-                  callbackUrl: '/',
-                })
-              }
-              disabled={!email || !password}
-              className='disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+              onClick={() => router.push('/auth/signup')}
+              className='font-semibold leading-6 text-indigo-400 hover:text-indigo-300'
             >
-              Sign in
+              Sign Up
             </button>
-          </div>
+          </p>
         </div>
-
-        <p className='mt-10 text-center text-sm text-gray-400'>
-          Not a member?{' '}
-          <button
-            onClick={() => router.push('/auth/signup')}
-            className='font-semibold leading-6 text-indigo-400 hover:text-indigo-300'
-          >
-            Sign Up
-          </button>
-        </p>
       </div>
     </div>
   )
