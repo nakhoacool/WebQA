7235f8ce59bc5acfcb84bb4ea5c6ead74bf82e6a
Khoa Nguyen <77623180+nakhoacool@users.noreply.github.com>
2024-02-14 20:53:14 +0700

    update chat bubble

--- client/components/component/chat-bubble.tsx
+++ client/components/component/chat-bubble.tsx
@@ -10,26 +10,24 @@ interface ChatBubbleProps {
 
 const ChatBubble: React.FC<ChatBubbleProps> = ({ data }) => {
   return (
-    <div className={`p-4 flex ${data.role === 'bot' ? '' : 'justify-end'}`}>
-      {data.role === 'bot' && (
-        <div className='rounded-full h-8 w-8 flex items-center justify-center mr-2'>
-          <RobotIcon />
+    <div
+      className={`p-4 chat ${data.role === 'bot' ? 'chat-start' : 'chat-end'}`}
+    >
+      <div className='chat-image avatar'>
+        <div className='w-9 rounded-full'>
+          {data.role === 'bot' ? <RobotIcon /> : <UserIcon />}
         </div>
-      )}
+      </div>
+      <div className='chat-header'>{data.role === 'bot' ? 'Bot' : 'User'}</div>
       <div
-        className={`rounded-lg w-full ${
-          data.role === 'bot' ? 'bg-gray-700 max-w-100' : 'bg-blue-600 max-w-80'
-        } px-4 py-2 inline-block text-base text-justify`}
+        className={`chat-bubble text-white ${
+          data.role === 'user' ? 'chat-bubble-info' : ''
+        }`}
       >
         {data.content}
       </div>
-      {data.role === 'user' && (
-        <div className='rounded-full h-8 w-8 flex items-center justify-center ml-2'>
-          <UserIcon />
-        </div>
-      )}
     </div>
   )
 }
 
-export default ChatBubble;
+export default ChatBubble
