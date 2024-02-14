ab152c88cdf02b20d811577fc0b1c6740f0a751f
dolphin4438 <www.trunghieu314@gmail.com>
2024-02-15 01:17:19 +0700

    added: learn agent

--- core/src/config.py
+++ core/src/config.py
@@ -1,9 +1,10 @@
 import os
 from elasticsearch import Elasticsearch
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
 class Configuration:
     """
-        This class responsible for loading API key, ES connection
+        This class responsible for loading API key, ES connection, LLM
     """
 
     def __init__(self):
@@ -41,6 +42,20 @@ class Configuration:
             key = f.read()
         return key.strip()
 
+    def load_tavily_token(self):
+        """
+            load tavily API token
+            @return the token
+        """
+        with open(self.path+"/tavily") as f:
+            key = f.read()
+        return key.strip()
+
+    def set_tavily_token(self):
+        token = self.load_tavily_token()
+        os.environ["TAVILY_API_KEY"] = token
+        return
+
     def enable_tracing(self, project:str):
         """
             enable tracing on langsmith.
@@ -51,7 +66,7 @@ class Configuration:
         os.environ["LANGCHAIN_API_KEY"]=self.load_langsmith_token()
         os.environ["LANGCHAIN_PROJECT"]=project.strip()
         return
-    
+
     def load_elasticsearch_connection(self) -> Elasticsearch:
         """
             load elastic search connection from hosted cloud
@@ -67,3 +82,28 @@ class Configuration:
             basic_auth=auth, 
             ca_certs=f'{self.env_path}/../.keys/ca.crt')
         return es
+    
+    def get_gemini_pro(self, convert_system_message:bool) -> ChatGoogleGenerativeAI:
+        """
+            get an instance of gemini pro
+
+            @return gemini chat model
+        """
+        chat_model = ChatGoogleGenerativeAI(
+            model="gemini-pro", 
+            temperature=0, 
+            google_api_key=self.load_gemini_token(),
+            convert_system_message_to_human=convert_system_message
+        )
+        return chat_model
+    
+    def get_gemini_embeddings(self) -> GoogleGenerativeAIEmbeddings:
+        """
+            get an instance of gemini embedding model
+
+            @return embedding model
+        """
+        model = GoogleGenerativeAIEmbeddings(
+            model="models/embedding-001", 
+            google_api_key=self.load_gemini_token())
+        return model
