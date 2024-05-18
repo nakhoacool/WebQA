ad199a00696114c8f0b85bd1db4ea9d519a81bcb
dolphin4438 <www.trunghieu314@gmail.com>
2024-05-18 11:32:44 +0700

    [RAG] web server bug, empty reply

--- core/app.py
+++ core/app.py
@@ -4,30 +4,13 @@ from src.config import Configuration
 from src.service.applog import AppLogService
 from langsmith.run_helpers import traceable
 import os
+from src.rag.hybrid_rank_rag import HybridRFFGeminiRAG
 from flask_cors import CORS
 from src.service.provider import ProviderService
-from src.robot import RAGRobot
-from src.service.googledrive import GoogleDriveService
-from typing import Dict
 
 # When call API, please check the "status" field first
 # 200 is success, else is error
 
-class RobotManager:
-    """
-        This class is designed to manage multiple robots for multiple users.
-        Simply put, it get the correct robot that serving for a particular user.
-    """
-    def __init__(self) -> None:
-        self.robots: Dict[str, RAGRobot] = {}
-        self.provider = ProviderService()
-        return
- 
-    def get_robot(self, id: str) -> RAGRobot:
-        if id in self.robots:
-            return self.robots[id]
-        self.robots[id] = RAGRobot(provider=self.provider)
-        return self.robots[id]
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -35,8 +18,10 @@ def create_app(test_config=None):
     CORS(app=app)
     log_service = AppLogService(name="app.log")
     config = Configuration()
-    config.enable_tracing("DEMO")
-    MANAGER = RobotManager()
+    config.enable_tracing("PARA_BOT_DEMO")
+    app_provider = ProviderService()
+    app_config = app_provider.categories.full_data
+    RAG_BOT = HybridRFFGeminiRAG(provider=app_provider, rag_config=app_config, update_notification_func=None)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -64,13 +49,6 @@ def create_app(test_config=None):
         data = {"question": "What is life", "answer": answer, "status": 200}
         return data
 
-    @app.route("/list_gdrive/")
-    def getFileListFromGDrive():
-        selected_fields="files(id,name,webViewLink)"
-        g_drive_service=GoogleDriveService().service
-        list_file=g_drive_service.files().list(fields=selected_fields).execute()
-        return {"files":list_file.get("files")}
-
 
     @app.route("/qa", methods=["POST"])
     def answer_major():
@@ -88,20 +66,10 @@ def create_app(test_config=None):
             return {"status": 404}
         try:
             question = request.form.get("question")
-            # userid = request.form.get("userid")
-            userid = "hada121sa"
-            bot = MANAGER.get_robot(id=userid)
-            rag_resp = bot.answer(question.strip())
-            doc = rag_resp.document
+            rag_resp = RAG_BOT.ask_rag(question.strip())
             data = {
                 "question": question, 
                 "answer": rag_resp.answer,
-                "doc": {
-                    "id": doc.id,
-                    "title": doc.title,
-                    "content": doc.content,
-                    "source": doc.source,
-                },
                 "category": rag_resp.category, 
                 "status": 200, 
                 "notfound": rag_resp.is_notfound()}
@@ -113,4 +81,4 @@ def create_app(test_config=None):
     return app
 
 if __name__ == '__main__':
-    create_app().run(host="0.0.0.0", port=int("5000"), debug=True)
+    create_app().run(host="0.0.0.0", port=int("5001"), debug=True)
--- test_POST.sh
+++ test_POST.sh
@@ -4,5 +4,5 @@ read QUES
 if [[ -z "$QUES" ]]; then
 	QUES="tư vấn ngành CNTT"
 fi
-echo -e `curl -d "question=$QUES" http://127.0.0.1:5000/qa` >> rag_test.txt
+echo -e `curl -d "question=$QUES" http://127.0.0.1:5001/qa` >> rag_test.txt
 cat ./rag_test.txt
